478a788fd963b4e17824613e8dd6e7d3b8cab6a8
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-05-07 17:16:05 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/growhubSite/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
