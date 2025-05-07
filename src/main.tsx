3c9068383da0f5974b154bb32efe6fb0850064dd
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-05-07 19:22:13 +0200

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "../variables.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const basename = '/growhubSite';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={basename}>
+    <BrowserRouter basename={basename}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
